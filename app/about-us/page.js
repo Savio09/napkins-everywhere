23f56bae174c8384a8a8daa63d55f96c23c88aaf
Andrea <andreamassukado@gmail.com>
2025-05-23 02:42:35 +0200

    Starting About Us Page

--- app/about-us/page.js
+++ app/about-us/page.js
@@ -1,7 +1,21 @@
 export default function About() {
   return (
     <div>
-      <h1>This is the about page</h1>
+      <p className="font-bold text-[1.2rem] mb-15 max-w-[600px]">About Us</p>
+      <h1 className=" max-w-[500px] text-[5rem] mb-10">From Magazine to Interactive 
+        Art: The Evolution of Napkins</h1>
+        
+      <p className="font-bold text-[1.2rem] mb-15 max-w-[600px]">Our story dates back 
+        to April of 2022, where four students from Minerva University came together 
+        to develop and share the beautiful stories of their classmates all around the world.</p>
+      <p className="font-bold text-[1.2rem] mb-15 max-w-[600px]">
+      In August 2022, Napkins published its first issue and received overwhelmingly positive 
+      feedback, both from students and professionals from outside the Minerva community.</p>
+      
+      <p className="font-bold text-[1.2rem] mb-15 max-w-[600px]">
+      At Napkins Magazine, we proudly publish creative works from the talented individuals 
+      associated with Minerva University, including students, staff, and alumni. We publish 
+      three times a year, during the Fall, Spring, and Summer seasons. We hibernate in the winter. 🐻</p>
     </div>
   );
 }
