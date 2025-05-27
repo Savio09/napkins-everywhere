8842100d03728deb5322e6b14739beb569abbdcc
Andrea <andreamassukado@gmail.com>
2025-05-27 23:42:16 +0200

    About Us: Fixing margins
    What is left: adding grids, changing text colour, meet the team part
    
    also, it would be nice to find a prettier font

--- app/about-us/page.js
+++ app/about-us/page.js
@@ -1,8 +1,8 @@
 export default function About() {
   return (
-    <div>
-      <p className="font-bold text-[1.2rem] mb-15 max-w-[600px]">About Us</p>
-      <h1 className=" max-w-[500px] text-[5rem] mb-10">From Magazine to Interactive 
+    <div className="bg-[#FFFFFF] min-h-screen px-8 py-12">
+      <p className="font-normal text-black text-[1.2rem] mb-12 ml-[10vw]">About Us</p>
+      <h1 className=" max-w-[600px] text-[#0171ad] text-[3.2rem] mb-10 ml-[10vw]">From Magazine to Interactive 
         Art: The Evolution of Napkins</h1>
         
       <p className="font-bold text-[1.2rem] mb-15 max-w-[600px]">Our story dates back 
@@ -16,6 +16,26 @@ export default function About() {
       At Napkins Magazine, we proudly publish creative works from the talented individuals 
       associated with Minerva University, including students, staff, and alumni. We publish 
       three times a year, during the Fall, Spring, and Summer seasons. We hibernate in the winter. 🐻</p>
+
+      <p className="font-bold text-[1.2rem] mb-15 max-w-[600px]">
+      With our cornerstone set, we also began to look beyond being a magazine and experimented 
+      with interactive art exhibitions to further our mission.
+      
+      We realized what is unique about us is that we are a group of young adults from 10+ countries who
+      have the privilege of living in new countries every semester. We carry with us a wealth of 
+      perspectives and creative energy that can coalesce into something powerful and beautiful. 🐻</p>
+      
+      <p className="font-bold text-[1.2rem] mb-15 max-w-[600px]">So, we talk with people from art galleries 
+        and creative spaces in the cities we live in. We talk with artists, educators, professors, and 
+        lovers of the arts. We share with them Napkins and our dreams over cups of coffee and sometimes tea.
+        
+        Our events focus on making arts accessible and enjoyable for all. We are driven by our mission to power 
+        and propel interdisciplinary imagination through the arts.
+        
+        Join us on this exciting journey as we celebrate the diverse and innovative artistic expressions 
+        from different communities! Our Instagram is where you’ll see action as it happens :)</p>
+    
     </div>
+    
   );
 }
